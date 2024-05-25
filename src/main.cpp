1b47102b0e963f7fa7af6834f0a5290051f421e2
Tino Göhlert <tino.gohlert@lovoo.com>
2024-05-25 21:24:19 +0200

    change path

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main()
 
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    app.LoadMap("/Users/tinogohlert/Downloads/quake_map_source/START.MAP");
+    app.LoadMap("/path/to/your/level.map");
     auto end_time = std::chrono::high_resolution_clock::now();
     auto time = end_time - start_time;
     std::cout << "map compiled in: " << time / std::chrono::milliseconds(1) << " ms\n";
